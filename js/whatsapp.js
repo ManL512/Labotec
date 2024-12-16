32ce3bc376911714d8afd531e42934dd1b91753d
ManL512 <miguelnavas933@gmail.com>
2024-12-16 17:46:33 -0500

    iconos version final

---
+++ js/whatsapp.js
@@ -0,0 +1,13 @@
+const whatsappIcon = document.getElementById("whatsappIcon");
+const whatsappBox = document.getElementById("whatsappBox");
+const closeChatBox = document.getElementById("closeChatBox");
+
+// Mostrar la caja de chat al hacer clic en el icono
+whatsappIcon.addEventListener("click", () => {
+    whatsappBox.style.display = "block";
+});
+
+// Cerrar la caja de chat al hacer clic en el botón de cerrar
+closeChatBox.addEventListener("click", () => {
+    whatsappBox.style.display = "none";
+});
