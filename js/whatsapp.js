9240e933e5bb6e48d0bfc29a045b1f5618515f5b
ManL512 <miguelnavas933@gmail.com>
2024-12-17 17:45:54 -0500

    logica

--- js/whatsapp.js
+++ js/whatsapp.js
@@ -11,3 +11,20 @@ whatsappIcon.addEventListener("click", () => {
 closeChatBox.addEventListener("click", () => {
     whatsappBox.style.display = "none";
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const languageLinks = document.querySelectorAll(".language-change");
+
+    languageLinks.forEach(link => {
+        link.addEventListener("click", function (e) {
+            e.preventDefault();
+            const href = this.getAttribute("href");
+
+            document.body.classList.add("page-transition"); // Aplica el efecto
+
+            setTimeout(() => {
+                window.location.href = href; // Redirige después de la animación
+            }, 500); // Tiempo coincidente con la duración de la animación
+        });
+    });
+});
